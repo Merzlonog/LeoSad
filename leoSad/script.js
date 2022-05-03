1172d7c2b023463b2c4288b292013ad01a2a0e46
Yaroslav Romanovich <83413652+Merzlonog@users.noreply.github.com>
2022-05-03 16:17:35 +0300

    Add files via upload

--- leoSad/script.js
+++ leoSad/script.js
@@ -1,7 +1,42 @@
 
-$(document).ready(function() {
-    $('.header__burger').click(function(event){
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
         $('.header__burger,.header__menu').toggleClass('active');
         $('.body').toggleClass('lock');
     });
 });
+
+$(function () {
+    $('.multiple-items').slick({
+        dots: true,
+        infinite: true,
+        dots:false,
+        autoplay: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+})
